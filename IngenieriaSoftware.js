6ef25fb158505169ba3a516e9ecf989981d9558b
Sebastian Fernando Gomez Huanco <seabastian.gomez@uab.edu.bo>
2025-05-29 18:17:44 -0400

    commit

--- IngenieriaSoftware.js
+++ IngenieriaSoftware.js
@@ -8,6 +8,7 @@ console.log("10. Elia Chacmi Paco");
 console.log("14. Ricardo Angel Duran Mamani");
 console.log("15. Jhurguen Ergueta Mamani");
 console.log("16. Jhammil Alcides Leonardo Mamani");
-console.log("17. Juan David Guzmn Zeballos");
+console.log("17. Sebastian Fernando Gomez Huanco");
+console.log("18. Juan David Guzmn Zeballos");
 console.log("27. Jhosep Andrei Mendoza Coca");
 console.log("40. Gustavo Rojas Valdivia");
