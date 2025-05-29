d9c2ff3aef4f4a95651f156972b0853f12a1306c
Miguel Blasquez <migoga2015@gmail.com>
2025-05-29 17:51:52 -0400

    mi commit

--- IngenieriaSoftware.js
+++ IngenieriaSoftware.js
@@ -1,10 +1,10 @@
 console.log("INGENIERÍA DE SOFTWARE");
 console.log("2. Galia Almaraz Jaillita");
+console.log("5. Miguel Alejandro Blasquez Iquiza");
 console.log("6.Jhoseb Pablo Cabrera Condori");
 console.log("8. Luis Carvajal Hinojosa");
 console.log("10. Elia Chacmi Paco");
 console.log("14. Ricardo Angel Duran Mamani");
 console.log("17. Juan David Guzmn Zeballos");
 console.log("40. Gustavo Rojas Valdivia");
-
 
