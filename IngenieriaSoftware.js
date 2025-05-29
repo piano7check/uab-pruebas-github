5df045c6f79e763310ec3a05c67548fe696eb8d0
brayan Munoz <brayan.munoz@uab.edu.bo>
2025-05-29 19:03:15 -0400

    Nombre main anadido

--- IngenieriaSoftware.js
+++ IngenieriaSoftware.js
@@ -13,5 +13,6 @@ console.log("17. Juan David Guzmn Zeballos");
 console.log("17. Sebastian Fernando Gomez Huanco");
 console.log("18. Juan David Guzmn Zeballos");
 console.log("27. Jhosep Andrei Mendoza Coca");
+console.log("28. Brayan Daniel Muñoz Mamani");
 console.log("37.Yhoselin Yessica Valero Huanca");
 console.log("40. Gustavo Rojas Valdivia");
