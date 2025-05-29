b92273134c292fd048981d642edb11b345ba25f9
efren2004 <henry.tandayamo@uab.edu.bo>
2025-05-29 18:43:51 -0400

    First_commit

--- IngenieriaSoftware.js
+++ IngenieriaSoftware.js
@@ -1,17 +1,1 @@
-console.log("INGENIERÍA DE SOFTWARE");
-console.log("1. Diego Anthony Adriazola Condori");
-console.log("2. Galia Almaraz Jaillita");
-console.log("5. Miguel Alejandro Blasquez Iquiza");
-console.log("6.Jhoseb Pablo Cabrera Condori");
-console.log("8. Luis Carvajal Hinojosa");
-console.log("10. Elia Chacmi Paco");
-console.log("14. Ricardo Angel Duran Mamani");
-console.log("15. Jhurguen Ergueta Mamani");
-console.log("16. Jhammil Alcides Leonardo Mamani");
 console.log("35. Henry Efren Tandayamo Imbago");
-console.log("17. Juan David Guzmn Zeballos");
-console.log("17. Sebastian Fernando Gomez Huanco");
-console.log("18. Juan David Guzmn Zeballos");
-console.log("27. Jhosep Andrei Mendoza Coca");
-console.log("37.Yhoselin Yessica Valero Huanca");
-console.log("40. Gustavo Rojas Valdivia");
