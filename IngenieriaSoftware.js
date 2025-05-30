3670805039c7f3ef23a2feaa8a701ff55940df28
Manuel Rocha <david.rocha@uab.edu.bo>
2025-05-30 14:41:24 -0400

    nombre agregado

--- IngenieriaSoftware.js
+++ IngenieriaSoftware.js
@@ -2,23 +2,21 @@ console.log("INGENIERÍA DE SOFTWARE");
 console.log("1. Diego Anthony Adriazola Condori");
 console.log("2. Galia Almaraz Jaillita");
 console.log("5. Miguel Alejandro Blasquez Iquiza");
-console.log("6.Jhoseb Pablo Cabrera Condori");
+console.log("6. Jhoseb Pablo Cabrera Condori");
 console.log("8. Luis Carvajal Hinojosa");
-console.log("9. Jose Rodrigo Cerrogrande Solano")
+console.log("9. Jose Rodrigo Cerrogrande Solano");
 console.log("10. Elia Chacmi Paco");
 console.log("13. Brian Diego Choque Lucana");
 console.log("14. Ricardo Angel Duran Mamani");
 console.log("15. Jhurguen Ergueta Mamani");
 console.log("16. Jhammil Alcides Leonardo Mamani");
-console.log("35. Henry Efren Tandayamo Imbago");
-console.log("17. Juan David Guzmn Zeballos");
-console.log("17. Sebastian Fernando Gomez Huanco");
-console.log("18. Juan David Guzmn Zeballos");
+console.log("17. Juan David Guzmán Zeballos");
+console.log("18. Sebastian Fernando Gomez Huanco");
 console.log("27. Jhosep Andrei Mendoza Coca");
 console.log("28. Brayan Daniel Muñoz Mamani");
-console.log("31. Alvaro Regulo Rivera Carhuani")
+console.log("31. Alvaro Regulo Rivera Carhuani");
+console.log("32. David Manuel Rocha Crespo");
+console.log("35. Henry Efren Tandayamo Imbago");
 console.log("36. Jhoel Titirico Charca");
-console.log("37.Yhoselin Yessica Valero Huanca");
+console.log("37. Yhoselin Yessica Valero Huanca");
 console.log("40. Gustavo Rojas Valdivia");
-
-
