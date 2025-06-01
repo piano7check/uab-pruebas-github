9e4471512ffbe54f6d6d5c2e13b035d32df770ab
Carlos <carlos.garcia@uab.edu.bo>
2025-06-01 16:22:16 -0400

    Conflicto resuelto

--- IngenieriaSoftware.js
+++ IngenieriaSoftware.js
@@ -1,5 +1,4 @@
 console.log("INGENIERÍA DE SOFTWARE");
-<<<<<<< HEAD
 console.log("1. Diego Anthony Adriazola Condori");
 console.log("2. Galia Almaraz Jaillita");
 console.log("5. Miguel Alejandro Blasquez Iquiza");
@@ -11,6 +10,7 @@ console.log("13. Brian Diego Choque Lucana");
 console.log("14. Ricardo Angel Duran Mamani");
 console.log("15. Jhurguen Ergueta Mamani");
 console.log("16. Jhammil Alcides Leonardo Mamani");
+console.log("16. Carlos Alberto Garcia Canaviri");
 console.log("35. Henry Efren Tandayamo Imbago");
 console.log("17. Juan David Guzmn Zeballos");
 console.log("17. Sebastian Fernando Gomez Huanco");
@@ -23,7 +23,3 @@ console.log("33. Josue Brayen Rojas Vasquez");
 console.log("36. Jhoel Titirico Charca");
 console.log("37.Yhoselin Yessica Valero Huanca");
 console.log("40. Gustavo Rojas Valdivia");
-=======
-console.log("16. Carlos Alberto Garcia Canaviri");
-
->>>>>>> carlos-garcia
